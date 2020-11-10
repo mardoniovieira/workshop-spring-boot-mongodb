1c0454867c874ba728ae6f9787505fc58c933791
Mardônio Vieira <mardoniovf@gmail.com>
2020-11-10 14:04:35 -0300

    Acrescentando comentarios aos posts

--- src/main/java/br/ufc/great/mardonio/workshopmongo/domain/Post.java
+++ src/main/java/br/ufc/great/mardonio/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package br.ufc.great.mardonio.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import br.ufc.great.mardonio.workshopmongo.dto.AuthorDTO;
+import br.ufc.great.mardonio.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -18,6 +21,8 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -72,6 +77,14 @@ public class Post implements Serializable{
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -96,5 +109,5 @@ public class Post implements Serializable{
 			return false;
 		return true;
 	}
-	
+
 }
