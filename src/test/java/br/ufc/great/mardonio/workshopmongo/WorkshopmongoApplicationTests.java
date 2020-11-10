f18c693b542c1f3407ece89aabe2e215e4813c39
Mardônio Vieira <mardoniovf@gmail.com>
2020-11-10 10:16:18 -0300

    Projeto criado

---
+++ src/test/java/br/ufc/great/mardonio/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufc.great.mardonio.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
